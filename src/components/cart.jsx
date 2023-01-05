b7af6c5ad7a14f1b68c3ef7b4f0f85ead5eb1e15
Tulpash <roman89203323606@gmail.com>
2023-01-05 17:07:12 +0300

    fix cart

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -6,13 +6,13 @@ const Cart = () => {
     return(
         <Link to={"/makko-spb/cart"}>
             <div className={"h-[50px] w-[50px] md:w-[100px] flex items-center justify-end group"}>
-                <div className={"h-[50px] w-[50px] relative flex items-center justify-center cursor-pointer rounded-md bg-transparent text-yellow-300 duration-200 group-hover:bg-yellow-300 group-hover:text-slate-600"}>
+                <div className={"h-[50px] w-[50px] flex items-center justify-center cursor-pointer rounded-md bg-transparent text-yellow-300 duration-200 group-hover:bg-yellow-300 group-hover:text-slate-600"}>
                     <span className="material-symbols-outlined text-[35px]">
                         shopping_cart
                     </span>
-                    <div className={'min-w-[20px] h-[20px] w-max absolute top-0 right-0 translate-x-[50%] translate-y-[-50%] rounded-[50%] bg-red-400 text-[12px] flex items-center justify-center'}>
+                    {/* <div className={'min-w-[20px] h-[20px] w-max absolute top-0 right-0 translate-x-[50%] translate-y-[-50%] rounded-[50%] bg-red-400 text-[12px] flex items-center justify-center'}>
                         {cartStorage.size()}
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </Link>
