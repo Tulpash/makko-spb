250ffc7caef8e8df2a5c69225b7fc17fee65828d
Tulpash <roman89203323606@gmail.com>
2023-01-15 20:40:36 +0300

    fixes

--- src/components/linkLogo.jsx
+++ src/components/linkLogo.jsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom'
 
 const LinkLogo = () => {
     return(
-        <Link to={'/'}>
+        <Link to={'/makko-spb'}>
             <img 
                 alt={''}
-                src={"https://menufyproduction.imgix.net/637441624273676818+346995.png?auto=compress,format&fit=max&w=1024&h=1024"} 
-                className={"h-full cursor-pointer"}    
+                src={"/makko-spb/images/logo.png"} 
+                className={"h-full cursor-pointer"}
             />
         </Link>
     )
