e0066901f7bbf640913a44793f4b11dc3f382a86
Tulpash <roman89203323606@gmail.com>
2023-01-15 20:37:40 +0300

    fixes

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 
 const Cart = () => {
     return(
-        <Link to={"/makko-spb/cart"}>
-            <div className={"h-[50px] w-[50px] md:w-[100px] flex items-center justify-end group"}>
-                <div className={"h-[50px] w-[50px] flex items-center justify-center cursor-pointer rounded-md bg-transparent text-yellow-300 duration-200 group-hover:bg-yellow-300 group-hover:text-slate-600"}>
+        <div className={"h-[50px] w-[50px] md:w-[100px] flex items-center justify-end"}>
+            <Link to={"/makko-spb/cart"}>
+                <div className={"h-[50px] w-[50px] flex items-center justify-center cursor-pointer rounded-md bg-transparent text-yellow-300 duration-200 hover:bg-yellow-300 hover:text-slate-600"}>
                     <span className={"material-symbols-outlined text-[35px]"}>
                         shopping_cart
                     </span>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
     )
 }
 
--- src/components/fullNavigation.jsx
+++ src/components/fullNavigation.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 const MenuButton = ({text, path, ico}) => {
     return(
         <Link to={path} className={'group px-2 py-2 cursor-pointer flex flex-row gap-2 items-center text-xl text-yellow-300 rounded-md duration-200 hover:text-slate-600 hover:bg-yellow-300'}>
-            <span className={"material-symbols-outlined text-[30px]"}>
+            {/* <span className={"material-symbols-outlined text-[30px]"}>
                 {ico}
-            </span>
+            </span> */}
             <span>
                 {text}
             </span>
